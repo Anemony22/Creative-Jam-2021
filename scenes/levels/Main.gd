c89b2babe16cb12b8026b9ce53ae9dd6079bedf1
Ben Clark <bennyjc01@gmail.com>
2021-06-30 17:51:59 +0800

    Character controller with torch

---
+++ scenes/levels/Main.gd
@@ -0,0 +1,20 @@
+extends Node
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func _unhandled_key_input(event):
+	if event is InputEventKey:
+		if event.is_action("ui_cancel"):
+			get_tree().quit()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
